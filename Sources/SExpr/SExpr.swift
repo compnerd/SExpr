0ec0e1d31565bbadfad9fdc2a3825e4d9c9cb11f
Saleem Abdulrasool <compnerd@compnerd.org>
2021-01-03 11:51:32 -0800

    SExpr: make `Symbol` and `SExpr` `Equatable` and `CustomStringConvertible`
    Make both the `Symbol` and `SExpr` conform to `CustomStringConvertible`
    which allows pretty-printing of the types.  Conform to `Equatable` to
    allow comparison of the types.

--- Sources/SExpr/SExpr.swift
+++ Sources/SExpr/SExpr.swift
@@ -5,13 +5,69 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
+import Foundation
+
 public enum Symbol {
   case int(Int)
   case double(Double)
   case string(String)
 }
 
+extension Symbol: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case let .int(integer):
+      return "\(integer)"
+    case let .double(real):
+      return "\(real)"
+    case let .string(string):
+      return string
+    }
+  }
+}
+
+extension Symbol: Equatable {
+  public static func == (_ lhs: Symbol, _ rhs: Symbol) -> Bool {
+    switch (lhs, rhs) {
+    case let (.int(lhs), .int(rhs)) where lhs == rhs:
+      return true
+    case let (.double(lhs), .double(rhs)) where lhs == rhs:
+      return true
+    case let (.string(lhs), .string(rhs)) where lhs == rhs:
+      return true
+    default:
+      return false
+    }
+  }
+}
+
 public enum SExpr {
   case atom(Symbol)
   case list([SExpr])
+}
+
+extension SExpr: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case let .atom(symbol):
+      return "\(symbol)"
+    case .list([]):
+      return "nil"
+    case let .list(expressions):
+      return "(\(expressions.map { "\($0)" }.joined(separator: " ")))"
+    }
+  }
+}
+
+extension SExpr: Equatable {
+  public static func == (_ lhs: SExpr, _ rhs: SExpr) -> Bool {
+    switch (lhs, rhs) {
+    case let (.atom(lhs), .atom(rhs)) where lhs == rhs:
+      return true
+    case let (.list(lhs), .list(rhs)) where lhs == rhs:
+      return true
+    default:
+      return false
+    }
+  }
 }
